679fb7ca9d0eb7c3d8bcd0bc6526deae33f15d75
Kareem Mahlees <kareemmahlees@gmail.com>
2023-05-19 09:56:07 +0300

    fix a typo

--- src/components/CodeExample.tsx
+++ src/components/CodeExample.tsx
@@ -10,7 +10,7 @@ const CodeExample: FC<CodeExampleProps> = ({}) => {
       <div className="flex flex-col items-center space-y-5 sm:flex-col sm:space-y-5 md:flex-row md:space-x-32">
         <div className="flex basis-1/2 flex-col items-start gap-7">
           <Zap width={64} height={64} />
-          <h2 className="text-3xl font-bold">Plazingly Fast.</h2>
+          <h2 className="text-3xl font-bold">Blazingly Fast.</h2>
           <p className="text-lg text-gray-600">
             Build with the language you want in a matter of seconds
           </p>
